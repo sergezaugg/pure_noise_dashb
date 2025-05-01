4d121da0e697f50b1d30b04a636281ff0a8bb885
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-01 13:03:05 +0200

    included statefulness for the h w sliders of lineplots

--- st_page_01.py
+++ st_page_01.py
@@ -25,8 +25,8 @@ if len(ss['resu']) > 0:
             make_plot(df = ss['dfresu'], width = ss["upar"]["par10"], height=ss["upar"]["par11"])
         with CB:
             st.text("Line-plot size")
-            _ = st.slider("Width",  min_value=100, max_value=1500, value=1000, step=100, key="wid10", on_change=update_ss, args=["wid10", "par10"])
-            _ = st.slider("Height", min_value=100, max_value=1500, value=500, step=100, key="wid11", on_change=update_ss, args=["wid11", "par11"])
+            _ = st.slider("Width",  min_value=100, max_value=1500, value=ss["upar"]["par10"], step=100, key="wid10", on_change=update_ss, args=["wid10", "par10"])
+            _ = st.slider("Height", min_value=100, max_value=1500, value=ss["upar"]["par11"], step=100, key="wid11", on_change=update_ss, args=["wid11", "par11"])
 
 
 
