62949a13ffa71d15d91a987a3a74755a55ca05bd
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-05 16:37:29 +0200

    good to go to dev dashboard

--- st_page_02.py
+++ st_page_02.py
@@ -21,10 +21,10 @@ with col_a:
     (2) choose the range of pure-noise-features to include into the feature space, and
     (3) choose the Random Forest parameters.
     Then you can start a simulation with the above parameters.
-    At each run, the dataset will be split 50/50 int0 train and test set.
-    The simulation results are accumulated in memory and shown in the tabs below.
+    At each run, the dataset will be split 50/50 into train and test set.
+    The simulation results are accumulated in memory and shown in the two bottom tabs.
     Note tha each time you run a simulation a new dataset is randomly sampled from the scenario.
-    Thus you can do multiple monte-carlo simulations (recommended).
+    Thus you can do multiple Monte-Carlo replicates (recommended).
     ''')
 
     st.image("pics/pic01.png")
@@ -36,12 +36,11 @@ with col_a:
     In this example, 3 scenarios were compared for two sets of RF parameters.
     We can see that the linearly-separable scenario (red) was more robust to pure-noise-features than the cross scenario (yellow).
     We also see that for the cat-eye scenario (green), the RF with max-feature = 1 collapses already with 10 pure-noise-features.
-    Raising max-feature to 10 considerably improved the robustness to pure-noise-features.
+    As expected, raising max-feature to 10 considerably improved the robustness to pure-noise-features.
     Caution, these results do not directly translate to real-world datasets.
-    But it is interesting to see that in certain situations, RF can handle dozens of pure-noise-features without any loss in predictive performance.
+    But it is interesting to see that in certain situations, RF can handle dozens of pure-noise-features with almost no loss in predictive performance.
     Have fun an try it yourself with new scenarios!                        
     ''')
-
 
     st.image("pics/pic02.png")
 
